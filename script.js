f281fdc49e0722450043ecfdac87ecde265e5e0d
Luis <lgd26308@gmail.com>
2020-08-31 21:13:08 -0700

    Added some initial JS structure

---
+++ script.js
@@ -0,0 +1,37 @@
+const quizEl = document.querySelector("quiz")
+const submitEl = document.querySelector("submit")
+const resultsEl = document.querySelector("results")
+
+
+
+const myQuestions = [
+    {
+      question: "Who invented JavaScript?",
+      answers: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which one of these is a JavaScript package manager?",
+      answers: {
+        a: "Node.js",
+        b: "TypeScript",
+        c: "npm"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which tool can you use to ensure code quality?",
+      answers: {
+        a: "Angular",
+        b: "jQuery",
+        c: "RequireJS",
+        d: "ESLint"
+      },
+      correctAnswer: "d"
+    }
+  ];
+
