4784cd8c2e0f3b036fab6a0fff7de3e415b140df
Luis <lgd26308@gmail.com>
2020-09-05 12:24:45 -0700

    Applied html structure & linked it to javascript

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 // create 4 buttons and give ids to insert here //
-const button1 = document.querySelector("")
-const button2 = document.querySelector("")
-const button3 = document.querySelector("")
-const button4 = document.querySelector("")
+const question = document.querySelector("question")
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const button4 = document.querySelector("#button4");
+const timeEl = document.querySelector(".time");
+
 
 let secondsLeft = 60;
 
 function setTime() {
-  let timerInterval = setInterval(function() {
+  let timerInterval = setInterval(function () {
     secondsLeft--;
-    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+    timeEl.textContent = ("Time left: " + secondsLeft);
 
-    if(secondsLeft === 0) {
+    if (secondsLeft === 0) {
       clearInterval(timerInterval);
-      sendMessage();
+      // sendMessage();
     }
 
   }, 1000);
@@ -22,99 +25,108 @@ function setTime() {
 const myQuestions = [
   {
     question: "Who invented JavaScript?",
-    answers: [
-       "Douglas Crockford",
-       "Sheryl Sandberg",
-       "Brendan Eich",
-       "something"
+    answers: ["Douglas Crockford",
+      "Sheryl Sandberg",
+      "Brendan Eich",
+      "something"
     ],
     correctAnswer: ""
   },
   {
     question: "Which one of these is a JavaScript package manager?",
-    answers: {
-      a: "Node.js",
-      b: "TypeScript",
-      c: "npm",
-      d: "something"
-    },
+    answers: [
+      "Node.js",
+      "TypeScript",
+      "npm",
+      "something"
+    ],
     correctAnswer: "c"
   },
   {
     question: "Which tool can you use to ensure code quality?",
-    answers: {
-      a: "Angular",
-      b: "jQuery",
-      c: "RequireJS",
-      d: "ESLint"
-    },
+    answers: [
+       "Angular",
+       "jQuery",
+       "RequireJS",
+       "ESLint"
+    ],
+    correctAnswer: "d"
+  },
+  {
+    question: "Which tool can you use to ensure code quality?",
+    answers: [
+       "Angular",
+       "jQuery",
+       "RequireJS",
+       "ESLint"
+    ],
     correctAnswer: "d"
   }
 ];
 
 
 let index = 0;
-let myQuestion = questionAnswer[0]
+let currentQuestion = myQuestions[0];
 nextQuestion();
 
 function nextQuestion() {
-  myQuestion = questionAnswer[index]
-question.textContent = myQuestion.question;
-button1.textContent = myQuestion.answer[0]
-button2.textContent = myQuestion.answer[1]
-button3.textContent = myQuestion.answer[2]
-button4.textContent = myQuestion.answer[3]
+  currentQuestion = myQuestions[index]
+  question.textContent = myQuestions.question;
+  button1.textContent = myQuestions.answers[0];
+  button2.textContent = myQuestions.answers[1];
+  button3.textContent = myQuestions.answers[2];
+  button4.textContent = myQuestions.answers[3];
 }
 
-button1.addEventListener("click", function(event) {
+button1.addEventListener("click", function (event) {
   event.preventDefault();
-  if (button1.textContent === myQuestion.correctAnswer) {
+  if (button1.textContent === myQuestions.correctAnswer) {
     alert("correct")
   }
   else {
     alert("wrong")
   }
   index++
-  nextQuestion(;)
+  nextQuestion()
 
 })
 
-button2.addEventListener("click", function(event) {
+button2.addEventListener("click", function (event) {
   event.preventDefault();
-  if (button2.textContent === myQuestion.correctAnswer) {
+  if (button2.textContent === myQuestions.correctAnswer) {
     alert("correct")
   }
   else {
     alert("wrong")
   }
   index++
-  nextQuestion(;)
+  nextQuestion()
 
 })
 
-button3.addEventListener("click", function(event) {
+button3.addEventListener("click", function (event) {
   event.preventDefault();
-  if (button3.textContent === myQuestion.correctAnswer) {
+  if (button3.textContent === myQuestions.correctAnswer) {
     alert("correct")
   }
   else {
     alert("wrong")
   }
   index++
-  nextQuestion(;)
+  nextQuestion()
 
 })
 
-button4.addEventListener("click", function(event) {
+button4.addEventListener("click", function (event) {
   event.preventDefault();
-  if (button4.textContent === myQuestion.correctAnswer) {
+  if (button4.textContent === myQuestions.correctAnswer) {
     alert("correct")
   }
   else {
     alert("wrong")
   }
   index++
-  nextQuestion(;)
+  nextQuestion()
 
 })
 
