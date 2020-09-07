b0dc6b44626e211b3f42444b23efb0065b2ac0dc
Luis <lgd26308@gmail.com>
2020-09-07 16:53:10 -0700

    made adjustments to js file

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // create 4 buttons and give ids to insert here //
-const question = document.querySelector("question")
+const questionEl = document.querySelector("#question")
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
@@ -8,21 +8,22 @@ const timeEl = document.querySelector(".time");
 
 
 let secondsLeft = 60;
+setTime();
 
 function setTime() {
   let timerInterval = setInterval(function () {
     secondsLeft--;
-    timeEl.textContent = ("Time left: " + secondsLeft);
+    timeEl.textContent = "Time left: " + secondsLeft;
 
     if (secondsLeft === 0) {
       clearInterval(timerInterval);
-      // sendMessage();
+      alert("You are out of time")
     }
 
   }, 1000);
 }
 
-const myQuestions = [
+let myQuestions = [
   {
     question: "Who invented JavaScript?",
     answers: ["Douglas Crockford",
@@ -30,7 +31,7 @@ const myQuestions = [
       "Brendan Eich",
       "something"
     ],
-    correctAnswer: ""
+    correctAnswer: "Sheryl Sandberg"
   },
   {
     question: "Which one of these is a JavaScript package manager?",
@@ -40,7 +41,7 @@ const myQuestions = [
       "npm",
       "something"
     ],
-    correctAnswer: "c"
+    correctAnswer: "Node.js"
   },
   {
     question: "Which tool can you use to ensure code quality?",
@@ -50,7 +51,7 @@ const myQuestions = [
        "RequireJS",
        "ESLint"
     ],
-    correctAnswer: "d"
+    correctAnswer: "Angular"
   },
   {
     question: "Which tool can you use to ensure code quality?",
@@ -60,73 +61,80 @@ const myQuestions = [
        "RequireJS",
        "ESLint"
     ],
-    correctAnswer: "d"
+    correctAnswer: "Angular"
   }
-];
+]
+
 
 
 let index = 0;
-let currentQuestion = myQuestions[0];
+let currentQuestion = myQuestions[index];
 nextQuestion();
 
 function nextQuestion() {
-  currentQuestion = myQuestions[index]
-  question.textContent = myQuestions.question;
+  currentQuestion = myQuestions[index];
+  questionEl.textContent = myQuestions.question;
   button1.textContent = myQuestions.answers[0];
   button2.textContent = myQuestions.answers[1];
   button3.textContent = myQuestions.answers[2];
   button4.textContent = myQuestions.answers[3];
+  
 }
 
 button1.addEventListener("click", function (event) {
   event.preventDefault();
   if (button1.textContent === myQuestions.correctAnswer) {
-    alert("correct")
+    alert("correct");
   }
   else {
-    alert("wrong")
+    alert("wrong");
   }
-  index++
-  nextQuestion()
+  index++;
+  nextQuestion();
 
 })
 
 button2.addEventListener("click", function (event) {
   event.preventDefault();
   if (button2.textContent === myQuestions.correctAnswer) {
-    alert("correct")
+    alert("correct");
   }
   else {
-    alert("wrong")
+    alert("wrong");
   }
-  index++
-  nextQuestion()
+  index++;
+  nextQuestion();
 
 })
 
 button3.addEventListener("click", function (event) {
   event.preventDefault();
   if (button3.textContent === myQuestions.correctAnswer) {
-    alert("correct")
+    alert("correct");
   }
   else {
-    alert("wrong")
+    alert("wrong");
   }
-  index++
-  nextQuestion()
+  index++;
+  nextQuestion();
 
 })
 
 button4.addEventListener("click", function (event) {
   event.preventDefault();
   if (button4.textContent === myQuestions.correctAnswer) {
-    alert("correct")
+    alert("correct");
   }
   else {
-    alert("wrong")
+    alert("wrong");
   }
-  index++
-  nextQuestion()
+  index++;
+  nextQuestion();
 
 })
+
+
+function renderQuestion() {
+
+}
 
